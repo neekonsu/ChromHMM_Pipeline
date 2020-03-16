8d1bfc1b231f8a466c7e47a984225541dbb710e7
Neekon Saadat <neekonsu@gmail.com>
2020-03-16 13:00:44 -0700

    ~(sources.config full links,  dependencies.config wget, PREPROCESS.sh full paths)

--- scripts/PREPROCESS.sh
+++ scripts/PREPROCESS.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #Set Directory vars: CLF=ChromosomeLengthFile IBD=InputBedDir CMFT=CellMarksFileTable OBD=OutputBedDir
-CLF="../ChromHMM/CHROMSIZES/hg19.txt"
-IBD="../DATA/raw/"
-CMFT="../DATA/CELLMARKFILETABLE/CMFT.txt"
-OBD="../DATA/binarized/"
+CLF="/usr/src/app/ChromHMM/CHROMSIZES/hg19.txt"
+IBD="/usr/src/app/DATA/raw/"
+CMFT="/usr/src/app/DATA/CELLMARKFILETABLE/CMFT.txt"
+OBD="/usr/src/app/DATA/binarized/"
 
-#Pull BED files from repositories; into ../DATA/compressed
-wget -P ../DATA/compressed -i ../DATA/sources.config
+#Pull BED files from repositories; into /usr/src/app/DATA/compressed
+wget -P /usr/src/app/DATA/compressed -i /usr/src/app/DATA/sources.config
 
-#Extract BED files from compressed format; from ../DATA/compressed/ to ../DATA/raw/
-for f in ../DATA/compressed/*.gz; do
+#Extract BED files from compressed format; from /usr/src/app/DATA/compressed/ to /usr/src/app/DATA/raw/
+for f in /usr/src/app/DATA/compressed/*.gz; do
   STEM=$(basename "${f}" .gz)
-  gunzip -c "${f}" > ../DATA/raw/"${STEM}"
+  gunzip -c "${f}" > /usr/src/app/DATA/raw/"${STEM}"
 done
 
 #Binarize BED files from raw format
 
-#java -mx4000M -jar ../ChromHMM/ChromHMM.jar BinarizeBed chromosomelengthfile $CLF inputbeddir $IBD cellmarkfiletable $CMFT outputbinarydir $OBD
+#java -mx4000M -jar /usr/src/app/ChromHMM/ChromHMM.jar BinarizeBed chromosomelengthfile $CLF inputbeddir $IBD cellmarkfiletable $CMFT outputbinarydir $OBD
