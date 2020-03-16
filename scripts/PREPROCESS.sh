fb985edda48e9c005e385266ba14de9cd7c27c80
Neekon Saadat <neekonsu@gmail.com>
2020-03-15 18:38:42 -0700

    ~(PREPROCESS.sh)

--- scripts/PREPROCESS.sh
+++ scripts/PREPROCESS.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-
+#Set Directory vars: CLF=ChromosomeLengthFile IBD=InputBedDir CMFT=CellMarksFileTable OBD=OutputBedDir
 CLF="../ChromHMM/CHROMSIZES/hg19.txt"
 IBD="../DATA/raw/"
 CMFT="../DATA/CELLMARKFILETABLE/CMFT.txt"
 OBD="../DATA/binarized/"
 
-#Pull BED files from repositories
+#Pull BED files from repositories; into ../DATA/compressed
 wget -P ../DATA/compressed -i ../DATA/sources.config
 
-#Extract BED files from compressed format
-for f in ../Data/compressed/*.gz; do
+#Extract BED files from compressed format; from ../DATA/compressed/ to ../DATA/raw/
+for f in ../DATA/compressed/*.gz; do
   STEM=$(basename "${f}" .gz)
   gunzip -c "${f}" > ../DATA/raw/"${STEM}"
 done
