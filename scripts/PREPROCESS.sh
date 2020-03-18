d7dd6daa2da32798c3f3bd482fbe52b2e36cccc9
Neekon Saadat <neekonsu@gmail.com>
2020-03-18 12:25:49 -0700

    ~(Verbose PREPROCESS.sh)

--- scripts/PREPROCESS.sh
+++ scripts/PREPROCESS.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 #Set Directory vars: CLF=ChromosomeLengthFile IBD=InputBedDir CMFT=CellMarksFileTable OBD=OutputBedDir
+COMPRESSED="/usr/src/app/DATA/compressed/"
 CLF="/usr/src/app/ChromHMM/CHROMSIZES/hg19.txt"
 IBD="/usr/src/app/DATA/raw/"
 CMFT="/usr/src/app/DATA/CELLMARKFILETABLE/CMFT.txt"
 OBD="/usr/src/app/DATA/binarized/"
 
-#Pull BED files from repositories; into /usr/src/app/DATA/compressed
-wget -P /usr/src/app/DATA/compressed -i /usr/src/app/DATA/sources.config
+#Check if compressed directory is empty
+if [ "$(ls -A $COMPRESSED)" ]; then
+  echo "✗ $COMPRESSED is not Empty, skipping dataset retreival"
+  sleep 1
+else
+  echo "$DIR is Empty, retreiving dataset"
+  sleep 1
+  #Pull BED files from repositories; into /usr/src/app/DATA/compressed
+  wget -P $COMPRESSED -i /usr/src/app/DATA/sources.config
+  sleep .5
+  echo "✓ Finished retreival, decompressing into $IBD"
+  sleep 1
+  #Extract BED files from compressed format; from /usr/src/app/DATA/compressed/ to /usr/src/app/DATA/raw/
+  for f in /usr/src/app/DATA/compressed/*.gz; do
+    STEM=$(basename "${f}" .gz)
+    gunzip -c "${f}" > /usr/src/app/DATA/raw/"${STEM}"
+    echo "✓ ${f} decompressed"
+  done
+fi
 
-#Extract BED files from compressed format; from /usr/src/app/DATA/compressed/ to /usr/src/app/DATA/raw/
-for f in /usr/src/app/DATA/compressed/*.gz; do
-  STEM=$(basename "${f}" .gz)
-  gunzip -c "${f}" > /usr/src/app/DATA/raw/"${STEM}"
-done
 
 #Binarize BED files from raw format
-
-java -mx4000M -jar /usr/src/app/ChromHMM/ChromHMM.jar BinarizeBed chromosomelengthfile /usr/src/app/ChromHMM/CHROMSIZES/hg19.txt inputbeddir /usr/src/app/DATA/raw/ cellmarkfiletable /usr/src/app/DATA/CELLMARKFILETABLE/CMFT.txt outputbinarydir /usr/src/app/DATA/binarized/
+java -mx4000M -jar /usr/src/app/ChromHMM/ChromHMM.jar BinarizeBed chromosomelengthfile "/usr/src/app/ChromHMM/CHROMSIZES/hg19.txt" inputbeddir "/usr/src/app/DATA/raw/" cellmarkfiletable "/usr/src/app/DATA/CELLMARKFILETABLE/CMFT.txt" outputbinarydir "/usr/src/app/DATA/binarized/"
 
 #Learn ChromHMM model with default parameters
-
 java -mx1600M -jar /usr/src/app/ChromHMM/ChromHMM.jar LearnModel /usr/src/app/DATA/BINARIZED/ OUTPUTSAMPLE 15 hg19
