d561c3a2488c4d70d7327e2bfd56f4a8c174176b
Neekon Saadat <neekonsu@gmail.com>
2020-03-18 16:55:29 -0700

    a.sdmn

--- scripts/PREPROCESS.sh
+++ scripts/PREPROCESS.sh
@@ -3,15 +3,15 @@
 COMPRESSED="/usr/src/app/DATA/compressed/"
 CLF="/usr/src/app/ChromHMM/CHROMSIZES/hg19.txt"
 IBD="/usr/src/app/DATA/raw/"
-CMFT="/usr/src/app/DATA/CELLMARKFILETABLE/CMFT.txt"
+CMFT="/usr/src/app/DATA/CELLMARKFILETABLE/CMFT.tsv"
 OBD="/usr/src/app/DATA/binarized/"
 
 #Check if compressed directory is empty
 if [ "$(ls -A $COMPRESSED)" ]; then
-  echo "✗ $(echo $COMPRESSED) is not Empty, skipping dataset retreival"
+        echo "✗ $(echo $COMPRESSED) is not Empty, skipping dataset retreival"
   sleep 1
 else
-  echo "$(echo $COMPRESSED) is Empty, retreiving dataset"
+        echo "$(echo $COMPRESSED) is Empty, retreiving dataset"
   sleep 1
   #Pull BED files from repositories; into /usr/src/app/DATA/compressed
   wget -P $COMPRESSED -i /usr/src/app/DATA/sources.config
@@ -26,9 +26,8 @@ else
   done
 fi
 
-
 #Binarize BED files from raw format
-java -mx4000M -jar /usr/src/app/ChromHMM/ChromHMM.jar BinarizeBed chromosomelengthfile "/usr/src/app/ChromHMM/CHROMSIZES/hg19.txt" inputbeddir "/usr/src/app/DATA/raw/" cellmarkfiletable "/usr/src/app/DATA/CELLMARKFILETABLE/CMFT.txt" outputbinarydir "/usr/src/app/DATA/binarized/"
+java -mx4000M -jar /usr/src/app/ChromHMM/ChromHMM.jar BinarizeBed $CLF $IBD $CMFT $OBD
 
-#Learn ChromHMM model with default parameters and paralellization
-java -mx1600M -jar /usr/src/app/ChromHMM/ChromHMM.jar LearnModel -p 0 /usr/src/app/DATA/BINARIZED/ OUTPUTSAMPLE 15 hg19
+#Learn ChromHMM model with default parameters
+java -mx1600M -jar /usr/src/app/ChromHMM/ChromHMM.jar LearnModel /usr/src/app/DATA/BINARIZED/ OUTPUTSAMPLE 15 hg19
