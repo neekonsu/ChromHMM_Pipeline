48514fade812ec111bcd2074384bb05eb9d16c07
Neekon Saadat <neekonsu@gmail.com>
2020-03-19 12:21:41 -0700

    ✓(finished PREPROCESS.sh v1)

--- scripts/PREPROCESS.sh
+++ scripts/PREPROCESS.sh
@@ -4,7 +4,8 @@ COMPRESSED="/usr/src/app/DATA/compressed/"
 CLF="/usr/src/app/ChromHMM/CHROMSIZES/hg19.txt"
 IBD="/usr/src/app/DATA/raw/"
 CMFT="/usr/src/app/DATA/CELLMARKFILETABLE/CMFT.tsv"
-OBD="/usr/src/app/DATA/binarized/"
+OBD="/usr/src/app/DATA/BINARIZED/"
+OUTPUTSAMPLE="/usr/src/app/DATA/OUTPUTSAMPLE/"
 
 #Check if compressed directory is empty
 if [ "$(ls -A $COMPRESSED)" ]; then
@@ -30,4 +31,4 @@ fi
 java -mx4000M -jar /usr/src/app/ChromHMM/ChromHMM.jar BinarizeBed $CLF $IBD $CMFT $OBD
 
 #Learn ChromHMM model with default parameters
-java -mx1600M -jar /usr/src/app/ChromHMM/ChromHMM.jar LearnModel /usr/src/app/DATA/BINARIZED/ OUTPUTSAMPLE 15 hg19
+java -mx1600M -jar /usr/src/app/ChromHMM/ChromHMM.jar LearnModel $OBD $OUTPUTSAMPLE 15 hg19
